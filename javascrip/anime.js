777583dfc9979e9ec1b13b79335d27df93987561
Dung Hoang <dunghoang22102003@gmail.com>
2022-05-15 09:44:25 +0700

    mass

---
+++ javascrip/anime.js
@@ -0,0 +1,15 @@
+function click1() {
+    document.getElementById('test-1').style.display = 'block'
+    document.getElementById('test-2').style.display = 'none'
+    document.getElementById('test-3').style.display = 'none'
+}
+function click2() {
+    document.getElementById('test-1').style.display = 'none'
+    document.getElementById('test-2').style.display = 'block'
+    document.getElementById('test-3').style.display = 'none'
+}
+function click3() {
+    document.getElementById('test-1').style.display = 'none'
+    document.getElementById('test-2').style.display = 'none'
+    document.getElementById('test-3').style.display = 'block'
+}
